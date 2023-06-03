1cabaf9150e0904ad5c6075926af45c093c37a42
Ivan Mieres <mieres.iv@gmail.com>
2023-06-03 15:55:49 -0300

    Proyect Created

---
+++ src/test/java/com/hackacode/themepark/ThemeparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackacode.themepark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
